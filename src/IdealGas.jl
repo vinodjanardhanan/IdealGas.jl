24a010e306eaa0019feda68e14c7690abf443c36
vinodjanardhanan <vj@che.iith.ac.in>
2022-08-18 09:23:04 +0530

    Code documentation changes

--- src/IdealGas.jl
+++ src/IdealGas.jl
@@ -199,14 +199,14 @@ end
 Calculate the specific heat of pure species J/mol-K
 #   Usage-1:
     cp(thermo,T) 
--   'thermo::NASAThermo': NASAThermo of the species
--   'T::Float64': Temperature in K at which the property is required    
+-   thermo::NASAThermo: NASAThermo of the species
+-   T::Float64: Temperature in K at which the property is required    
 # Usage-2:
     cp(sp,T,thermo,ig)
--   'sp::String' : species name
--   'T::Float64' : Temperature K
--   'thermo::SpeciesThermoObj' : Structure of SpeciesThermoObj
--   'ig::Gasphase ' : Gasphase object
+-   sp::String : species name
+-   T::Float64 : Temperature K
+-   thermoObj::SpeciesThermoObj : Structure of SpeciesThermoObj
+-   species_list::Array{String,1}  : List of species 
 """
 function cp(thermo::NASAThermo, T::Float64)
     TVec = [T^i for i in 0:4]    
@@ -225,10 +225,10 @@ Calculates the enthalpy of pure species J/mol
 -   'T::Float64': Temperature in K at which the property is required   
 # Usage-2:
     H(sp,T,thermo,ig)
--   'sp::String' : species name
--   'T::Float64' : Temperature K
--   'thermo::SpeciesThermoObj' : Structure of SpeciesThermoObj
--   'ig::Gasphase ' : Gasphase object
+-   sp::String : species name
+-   T::Float64 : Temperature K
+-   thermoObj::SpeciesThermoObj : Structure of SpeciesThermoObj
+-   species_list::Array{String,1}  : List of species 
 """
 function H(thermo::NASAThermo, T::Float64)
     TVec = [1, T/2.0, T^2/3.0, T^3/4.0, T^4/5.0, 1.0/T]    
@@ -242,14 +242,14 @@ S(thermo::NASAThermo, T::Float64)
 Calculates the entropy of pure species J/mol-K
 #   Usage-1:
     S(thermo,T)
--   'thermo::NASAThermo': NASAThermo of the species
--   'T::Float64': Temperature in K at which the property is required    
+-   thermo::NASAThermo: NASAThermo of the species
+-   T::Float64: Temperature in K at which the property is required    
 # Usage-2:
     S(sp,T,thermo,ig)
--   'sp::String' : species name
--   'T::Float64' : Temperature K
--   'thermo::SpeciesThermoObj' : Structure of SpeciesThermoObj
--   'ig::Gasphase ' : Gasphase object
+-   sp::String : species name
+-   T::Float64 : Temperature K
+-   thermo::SpeciesThermoObj : Structure of SpeciesThermoObj
+-   species_list::Array{String,1}  : List of species 
 """
 function S(thermo::NASAThermo, T::Float64)
     TVec = [log(T), T, T^2/2.0, T^3/3.0, T^4/4.0]    
